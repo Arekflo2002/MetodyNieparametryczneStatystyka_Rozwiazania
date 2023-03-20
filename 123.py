ef7c58e4cd43e2a0f206b8cc7b0bc5ffb83e034f
Arkadiusz Florek <arekflo123@gmail.com>
2023-03-20 01:12:52 +0100

    #

--- 123.py
+++ 123.py
@@ -1,7 +1,29 @@
-from scipy.stats import *
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+import numpy as np
 
-num_data = []
+liczba_stopni = [1,20,100,1000,100000]
+alfa = 0.05
+n = 50
+liczba_symulacji = 1000
+moce_testow = []
 
-data = t.rvs(6,size=5)
+# Zebranie danych z rozkladu t - studenta
 
-print(data)
+for stopien in liczba_stopni:
+    odrzucenie_H0 = 0
+    for K in range(liczba_symulacji):
+        probka = stats.t.rvs(stopien,size=n)
+        # Standaryzacja danych i guess
+        stand_probka = (probka - np.mean(probka))/np.std(probka)
+        # Bede badal odsetek odrzucenia H0 przy 1000 symulacji 
+        # Jezeli pvalue jest < 0.05 to sa podstawy do orzucenia H0 wiec =+ zmienna 
+        if stats.shapiro(stand_probka).pvalue < alfa : 
+            odrzucenie_H0 += 1
+    
+    # Obliczamy ostateczny odsetek odrzucenia H0
+    moce_testow.append(odrzucenie_H0/liczba_symulacji)
+
+
+print(moce_testow)
+
