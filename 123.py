0fd90f8df571288a938e632721c6eef0f4bb553e
Arkadiusz Florek <arekflo123@gmail.com>
2023-03-20 01:57:47 +0100

    #

--- 123.py
+++ 123.py
@@ -4,26 +4,30 @@ import numpy as np
 
 liczba_stopni = [1,20,100,1000,100000]
 alfa = 0.05
-n = 50
+n = [10, 30, 50, 100, 300, 500, 1000]
 liczba_symulacji = 1000
 moce_testow = []
 
 # Zebranie danych z rozkladu t - studenta
 
 for stopien in liczba_stopni:
-    odrzucenie_H0 = 0
-    for K in range(liczba_symulacji):
-        probka = stats.t.rvs(stopien,size=n)
-        # Standaryzacja danych i guess
-        stand_probka = (probka - np.mean(probka))/np.std(probka)
-        # Bede badal odsetek odrzucenia H0 przy 1000 symulacji 
-        # Jezeli pvalue jest < 0.05 to sa podstawy do orzucenia H0 wiec =+ zmienna 
-        if stats.shapiro(stand_probka).pvalue < alfa : 
-            odrzucenie_H0 += 1
-    
-    # Obliczamy ostateczny odsetek odrzucenia H0
-    moce_testow.append(odrzucenie_H0/liczba_symulacji)
+    for nka in n:
+        odrzucenie_H0 = 0
+        moc_n_testu = []
+        for K in range(liczba_symulacji):
+            probka = stats.t.rvs(stopien,size=nka)
+            # Standaryzacja danych i guess
+            stand_probka = (probka - np.mean(probka))/np.std(probka)
+            # Bede badal odsetek odrzucenia H0 przy 1000 symulacji 
+            # Jezeli pvalue jest < 0.05 to sa podstawy do orzucenia H0 wiec =+ zmienna 
+            if stats.shapiro(stand_probka).pvalue < alfa : 
+                odrzucenie_H0 += 1
+        
+        # Obliczamy ostateczny odsetek odrzucenia H0
+        moc_n_testu.append(odrzucenie_H0/nka)
+
+    moce_testow.append(moc_n_testu)
 
 
-print(moce_testow)
-
+for moce in moce_testow:
+    print(moce)
