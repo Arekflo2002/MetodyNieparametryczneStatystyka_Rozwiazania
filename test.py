1b0de52ec028be5768ff76e32b2ef10013cbfab6
Arkadiusz Florek <arekflo123@gmail.com>
2023-04-19 16:57:53 +0200

    #

--- test.py
+++ test.py
@@ -3,4 +3,6 @@ import scipy.stats as stats
 
 probka = stats.chi2.rvs(5,size = 1000)
 
-podzial,bins = np.histogram(probka,bins)
+# Pewna strona sugeruje zeby uzyc 2*n^(2/5) przedzialow 
+podzial,bins = np.histogram(probka,bins='fd')
+print(podzial,"\n",bins)
