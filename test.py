a281b3c20532fc307439091d2545ed8bb4e8d3e6
Arkadiusz Florek <arekflo123@gmail.com>
2023-05-08 17:41:04 +0200

    #

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy.stats as stats
 import matplotlib.pyplot as plt
+import math
 
 dfs = 3
 probka = np.random.chisquare(dfs,size = 200)
@@ -9,26 +10,26 @@ probka = np.random.chisquare(dfs,size = 200)
 x = np.linspace(0, 50, 1000)
 
 # obliczenie PDF dla rozkładu chi-kwadrat
-
+odsetek = 0
 for i in range(50):
-    probka = stats.norm.rvs(size=200)
-    probka_t = stats.t.rvs(size=200,df=100)
+    probka = stats.norm.rvs(size=50)
+    probka_t = stats.t.rvs(size=50,df=100)
 
-    dol = min(min(probka),min(probka_t))
-    gora = max(max(probka),max(probka_t))
 
 
     # Podzielenie danych na przedzialy 
-
-    probka_podzial, rozdz = np.histogram(probka, bins=5,range=(dol,gora))
-
-    probka_podzial_t,rozdz_t = np.histogram(probka_t, bins=rozdz,range=(dol,gora))
-
-
-
+    
+    probka_podzial, rozdz = np.histogram(probka, bins='sturges')
     print(probka_podzial)
-    pval = stats.chisquare(probka_podzial_t,probka_podzial).pvalue
+    probka_podzial_t,rozdz_t = np.histogram(probka_t, bins=rozdz)
+    roznica = sum(probka_podzial) - sum(probka_podzial_t)
 
-    if pval == 'nan' or pval == 0.0:
-        print(123)
+    probka_podzial_t[0] += math.ceil(roznica/2)
+    probka_podzial_t[-1] += math.floor(roznica/2)
+
+    pval = stats.chisquare(probka_podzial_t,probka_podzial).pvalue
+    print(pval)
+    if pval < 0.05:
+        odsetek +=1 
+    
 
