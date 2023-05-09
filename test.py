9e44db89afee7e265ac6cb9cd357c509ad95c39a
Arkadiusz Florek <arekflo123@gmail.com>
2023-05-09 15:38:32 +0200

    Zadanie 1 ogarniete

--- test.py
+++ test.py
@@ -5,36 +5,39 @@ import math
 
 # wartości punktów, w których ma zostać obliczony PDF
 x = np.linspace(0, 50, 1000)
+np.random.seed(1234)
 
 # obliczenie PDF dla rozkładu chi-kwadrat
 odsetek = 0
-for i in range(4):
-    probka = stats.norm.rvs(size=100)
-    probka_t = stats.t.rvs(size=100,df=100)
+n=200
+for i in range(50):
+    probka = stats.norm.rvs(size=n)
+    probka_t = stats.t.rvs(size=n,df=200)
 
 
 
     # Podzielenie danych na przedzialy 
     
     probka_podzial, rozdz = np.histogram(probka, bins='auto')
-    print(rozdz,"\n:",probka_podzial)
 
-    probka_podzial_te = probka_podzial
-    rozdz_te = rozdz
+    id = np.nonzero(probka_podzial==0)[0]
+    probka_podzial = probka_podzial[probka_podzial!=0]
 
-    l1,l2 = np.array_split(probka_podzial,2)
-    l1 = l1[:l1.index]
+    rozdz = np.delete(rozdz,id)
 
-    # probka_podzial_t,rozdz_t = np.histogram(probka_t, bins=rozdz)
-    # roznica = sum(probka_podzial) - sum(probka_podzial_t)
+    probka_podzial_t,rozdz_t = np.histogram(probka_t, bins=rozdz)
+    roznica = sum(probka_podzial) - sum(probka_podzial_t)
     
 
-    # probka_podzial_t[0] += math.ceil(roznica/2)
-    # probka_podzial_t[-1] += math.floor(roznica/2)
+    probka_podzial_t[0] += math.ceil(roznica/2)
+    probka_podzial_t[-1] += math.floor(roznica/2)
 
-    # pval = stats.chisquare(probka_podzial_t,probka_podzial).pvalue
-    # print(pval)
-    # if pval < 0.05:
-    #     odsetek +=1 
+    if sum(probka_podzial) != sum(probka_podzial_t):
+        print(probka_podzial_t)
+
+    pval = stats.chisquare(probka_podzial_t,probka_podzial).pvalue
+    print(pval)
+    if pval < 0.05:
+        odsetek +=1 
     
-
+print(odsetek/50)
