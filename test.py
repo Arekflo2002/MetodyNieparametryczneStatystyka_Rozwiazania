5a8f1120ed8a1552c53148221245da61b5328b54
Arkadiusz Florek <arekflo123@gmail.com>
2023-05-13 20:18:09 +0200

    Sprzatanie po zad 2

--- test.py
+++ test.py
@@ -3,32 +3,20 @@ import scipy.stats as stats
 import matplotlib.pyplot as plt
 
 # ustalamy parametry symulacji
-n_simulations = 400
-degrees_of_freedom = [1, 2, 5, 10, 20, 50, 100]
+np.random.seed(seed=123)
 
-# ustalamy poziom istotności dla testu chi-kwadrat
-alpha = 0.05
+n = 200
 
-# tworzymy listy do przechowywania wyników symulacji
-rejections = []
-p_values = []
+# plt.hist(x, bins=10)
+# plt.show()
+odsetek = 0
+for i in range(400):
+    x = stats.t.rvs(size=n,df=1)
+    x = stats.t.cdf(x,df=1)
+    y = stats.uniform.rvs(size=n)
+    # plt.plot(y)
+    # plt.show()
+    if stats.kstest(x,'uniform').pvalue<0.05: odsetek +=1 
 
-# dla każdego stopnia swobody wykonujemy symulacje i zapisujemy wyniki
-for k in degrees_of_freedom:
-    rejection_count = 0
-    for i in range(n_simulations):
-        # generujemy próbki z rozkładu chi-kwadrat o k stopniach swobody
-        data = np.random.chisquare(k, size=100)
-        # wykonujemy test chi-kwadrat i zapisujemy wynik
-        _, p = stats.chisquare(data, f_exp=np.full_like(data, np.mean(data)))
-        p_values.append(p)
-        if p < alpha:
-            rejection_count += 1
-    rejections.append(rejection_count / n_simulations)
 
-# rysujemy wykres
-plt.plot(degrees_of_freedom, rejections)
-plt.xlabel('Stopnie swobody')
-plt.ylabel('Odsetek odrzuceń')
-plt.title('Test chi-kwadrat dla rozkładu chi-kwadrat')
-plt.show()
+print(odsetek/400)
